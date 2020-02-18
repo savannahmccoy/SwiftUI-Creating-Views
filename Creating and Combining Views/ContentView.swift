0f81f8f0d864b30b478e6f4f90e4a1d8fe79265f
Savannah McCoy <svm333@stern.nyu.edu>
2020-02-18 14:40:34 -0500

    Initial Commit

---
+++ Creating and Combining Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Creating and Combining Views
+//
+//  Created by Savannah McCoy on 2/18/20.
+//  Copyright © 2020 Savannah McCoy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
